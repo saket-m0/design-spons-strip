6e823d71c9ec1d102c6f329eb444f969ac3d2f98
Saket Mahajan <saketmahajan99@gmail.com>
2020-11-08 18:13:55 +0530

    Edited the ReadMe and added Assets and Final Folder

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# design-spons-strip
+# Design Spons Strip
+
+This repository helps the Entrepreneurship Cell, IIT Kharagpur to add the Spons Strips in their Posters.
+
+### Installation
+
+Activate the Virtual Environment using
+```sh
+VirtualEnv\Scripts\activate
+```
+Install the dependencies using
+```sh
+pip install -r requirements.txt
+```
+
+### Getting Started
+- Add the Poster and Spons Strip in the Assets folder
+- Run the index.py file
+- Enter the Name of the Poster (with extension)
+- Enter the Name of the Spons Strip image (with extension)
+- Enter the Name of Final Poster you which has spons strip attached to the poster (with extension)
+- You can find the Final poster in the Final Folder
--- index.py
+++ index.py
@@ -1,25 +1,32 @@
 import cv2
 
+
 def ResizeImage(image, width):
     image_resized = cv2.resize(image, (width, int(image.shape[0] / image.shape[1] * width)))
     return image_resized
 
+
 def ShowImage(image):
-    cv2.imshow('image', image) 
+    cv2.imshow('image', image)
     k = cv2.waitKey(0) & 0xFF
-    
-    # wait for ESC key to exit 
+
+    # wait for ESC key to exit
     if k == 27:  
-        cv2.destroyAllWindows() 
+        cv2.destroyAllWindows()
+
 
 def SaveImage(image, name):
-    cv2.imwrite(name,image) 
+    cv2.imwrite('Final/' + name, image)
+
 
 POSTER = input("Enter Name of the Poster: ")
 SPONS_STRIP = input("Enter Name of the Spons Strip: ")
 
-poster = cv2.imread(POSTER)
-spons_strip = cv2.imread(SPONS_STRIP)
+
+poster = cv2.imread('Assets/' + POSTER)
+spons_strip = cv2.imread('Assets/' + SPONS_STRIP)
+
+ShowImage(poster)
 
 poster_resized = ResizeImage(poster, 1000)
 spons_strip_resized = ResizeImage(spons_strip, 1000)
